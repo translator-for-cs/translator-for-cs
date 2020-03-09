f8337d98e20ad09f24ff2a06c30c0880c294903f
kl-erik <gusklier@student.gu.se>
2020-03-09 14:22:38 +0100

    scotty setup

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Network.Wai.Middleware.Cors
+import Web.Scotty
+
+import Control.Monad.IO.Class (liftIO)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = scotty 3000 $ do
+  middleware simpleCors
+  post "/" $ do
+    liftIO $ putStrLn "Hej"
+  -- TODO
+  -- post "/translate" $ do
+  --   query <- param "query"
+  --   w <- lookup query lexicon
+  --   liftIO $ putStrLn w
